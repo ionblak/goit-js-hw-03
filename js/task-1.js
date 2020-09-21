d39384e1160fbe76442aa9ab4dd72ee01c8175be
Igor Ionasku <ionaigor10@gmail.com>
2020-09-21 20:49:14 +0300

    completed 6 task

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,28 @@
+let message = "";
+const user = {
+  age: 20,
+  hobby: "html",
+  name: "Mango",
+  premium: true,
+};
+user.mood = "happy";
+user["full time"] = true;
+user.hobby = "skydiving";
+user.premium = false;
 
+const keys = Object.keys(user);
+
+for (const key of keys) {
+  message += `${key} : ${user[key]}\n`;
+}
+
+console.log(user);
+console.log(message);
+/*
+'age : 20
+hobby : skydiving
+name : Mango
+premium : false
+mood : happy
+"full time" : true
+' */
---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+const countTotalSalary = function (employees) {
+  "use strict";
+  employees = Object.values(employees);
+  let total = 0;
+  for (const salary of employees) {
+    total += salary;
+  }
+  return total;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  mango: 300,
+  poly: 250,
+  alfred: 450,
+};
+console.log(countTotalSalary(developers));
+// 1000
+
+const supports = {
+  kiwi: 200,
+  lux: 150,
+  chelsy: 150,
+};
+console.log(countTotalSalary(supports));
+// 500
